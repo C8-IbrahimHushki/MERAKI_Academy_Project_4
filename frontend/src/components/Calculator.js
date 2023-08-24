a005cdddc651ae27da4e37b10dfb3ebae92159c3
IbrahimHushki <hushki.ibrahim@gmail.com>
2023-08-24 21:46:52 +0300

    Added login function to frontend

--- frontend/src/components/Calculator.js
+++ frontend/src/components/Calculator.js
@@ -16,13 +16,25 @@ const Calculator = () => {
       <input name="height" placeholder="Height" />
       <p>cm</p>
       <label for="weight">Weight:&nbsp;</label>
-      <input name="weight" type="number" min="1" max="300" placeholder="Weight" />
+      <input
+        name="weight"
+        type="number"
+        min="1"
+        max="300"
+        placeholder="Weight"
+      />
       <p>kg</p>
       <label for="age">Age:&nbsp;</label>
       <input name="age" type="number" min="1" max="100" placeholder="Age" />
       <br />
       <label for="goal-weight">Goal Weight:&nbsp;</label>
-      <input name="goal-weight" type="number" min="20" max="300" placeholder="Goal Weight" />
+      <input
+        name="goal-weight"
+        type="number"
+        min="20"
+        max="300"
+        placeholder="Goal Weight"
+      />
       <br />
       <label for="goal">Goal:&nbsp;</label>
       <select name="goal">
@@ -34,6 +46,14 @@ const Calculator = () => {
       </select>
       <input type="checkbox" name="bodybuilding" />
       <label for="bodybuilding">Bodybuilding</label>
+      <br />
+      <label for="activity">How active are you on a daily basis?&nbsp;</label>
+      <select name="activity">
+        <option value="light">Lightly Active</option>
+        <option value="moderate">Moderately Active</option>
+        <option value="active">Active</option>
+        <option value="very">Very Active</option>
+      </select>
       <br />
       <button>Calculate</button>
     </div>
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,18 +1,65 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../App";
+import axios from "axios";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const {isLoggedIn,setIsLoggedIn } = useContext(Context);
+
+  const loggedUser = {
+    email: email,
+    password: password,
+  };
+
+  const loginUser = () => {
+    axios
+      .post(`http://localhost:5000/users/login`, loggedUser)
+      .then((response) => {
+        setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", isLoggedIn)
+        console.log(response);
+        navigate(-1)
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message)
+      });
+  };
+
   return (
     <div>
-        <Link to="/">Back</Link>
+      <Link to="/">Back</Link>
       <h2>Log In</h2>
       <label for="email">Email:&nbsp;</label>
-      <input name="email" type="email" placeholder="Email" />
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
       <br />
       <label for="password">Password:&nbsp;</label>
-      <input name="password" type="password" placeholder="Password" />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
       <br />
-      <button>Log In</button>
+      <button
+        onClick={() => {
+          loginUser();
+        }}
+      >
+        Log In
+      </button>
     </div>
   );
 };
